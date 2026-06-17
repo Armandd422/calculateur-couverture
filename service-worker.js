5886f53346180753b617a4e886318691b23a50d2
Armandd422 <armand.cutajar@gmail.com>
2026-06-17 16:54:15 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Couvr'Toit Métré - Service Worker v6
-const CACHE='couvrtoit-v10';
+const CACHE='couvrtoit-v11';
 const ASSETS=[
   './','./index.html','./manifest.json',
   './logo.png','./icon-192.png','./icon-512.png',
